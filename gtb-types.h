cde35e3675a83ca6874729a3d66eec78865b81df
Miguel <mballicora@gmail.com>
2010-10-29 13:54:30 -0500

    [0.3.0.15] Update readme.txt

---
+++ gtb-types.h
@@ -0,0 +1,23 @@
+#if !defined(H_GTBTYPES)
+#define H_GTBTYPES
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+#include "sysport.h"
+
+typedef unsigned 			sq_t;
+typedef unsigned char 		pc_t;
+typedef uint32_t			mv_t;
+typedef long int			tbkey_t;
+typedef unsigned short  	dtm_t;
+typedef size_t 				index_t;
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
+
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "0.3.0.14"
+#define VERSION "0.3.0.15"
 
