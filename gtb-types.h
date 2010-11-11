3f345f7e84f7fa19f6ca7608a18057d0eed7b96c
Miguel <mballicora@gmail.com>
2010-11-10 20:04:28 -0600

    [0.3.0.41] Fix dtm_t type, it should be unsigned

--- gtb-types.h
+++ gtb-types.h
@@ -7,10 +7,10 @@ extern "C" {
 #include "sysport.h"
 
 typedef uint32_t 			sq_t;
-typedef int8_t 				pc_t;
+typedef uint8_t 			pc_t;
 typedef uint32_t			mv_t;
 typedef int32_t				tbkey_t;
-typedef int16_t			  	dtm_t;
+typedef uint16_t		  	dtm_t;
 typedef int32_t				index_t;
 
 #define MAXINDEX_T			((1ul << 31)-1ul)
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "0.3.0.40"
+#define VERSION "0.3.0.41"
 
